15790ff9698850504b60ceaad8970e3c9f78f923
andrecesarino <andre.cesarino@gmail.com>
2021-03-27 10:20:36 -0300

    3 files modified

--- src/app/propostas.ts
+++ src/app/propostas.ts
@@ -1,22 +1,22 @@
 export const propostas = [
   {
     id: 1,
-    name: 'Expansão Padaria',
-    price: 50000,
+    nome: 'Expansão Padaria',
+    preco: 50000,
     juros: 0.07,
     description: 'Pretendo comprar maquinário para fabricar mais pães.'
   },
   {
     id: 2,
-    name: 'Confeitaria',
-    price: 70000,
+    nome: 'Confeitaria',
+    preco: 70000,
     juros: 0.12,
     description: 'Pretendo montar uma filial no Jardim América.'
   },
   {
     id: 3,
-    name: 'Cervejaria',
-    price: 40000,
+    nome: 'Cervejaria',
+    preco: 40000,
     juros: 0.09,
     description: 'Busco comprar mais dornas de fermentação para a produção.'
   }
